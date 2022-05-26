b89ab11a16835573864fb00da3526b31e54b987b
frcneto <frc_neto@yahoo.com.br>
2022-05-26 19:52:21 -0300

    retirado sudo e acrescento -y
    Retirado comando sudo da linha 17 e acrescentado o -y para o apt-get não solicitar confirmação

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 
-apt-get install \
+apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -14,7 +14,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
 
